7664e1e1bde68a8208b25b0e204bb7ee6cecff42
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-02 23:29:56 +0530

    getting problems in relative imports

---
+++ operation package/database_creator/create_db.py
@@ -0,0 +1,3 @@
+from ..model import models
+sqllite = models.sql_lite_db()
+sqllite.create_table()
--- operation package/utils/stacks.py
+++ operation package/utils/stacks.py
@@ -1,19 +1,19 @@
 class Stack:
     def __init__(self):
-        self.__array = [] # hiding the data using double underscore
+        self.__array = []  # hiding the data using double underscore
 
-    def push(self,element):
+    def push(self, element):
         self.__array.append(element)
         return self.__array
 
     def pop(self):
         return self.__array.pop()
-    
+
     def export_dict(self):
         dictionary = dict()
         for i in enumerate(self.__array):
-            dictionary[i[0]]=i[1]
+            dictionary[i[0]] = i[1]
         return dictionary
 
     def __repr__(self):
-        return str(self.__array[::-1]) # must be converted to string before printing
+        return str(self.__array[::-1])  # must be converted to string before printing
